4c6246c33032a3c4207334d87e348168b0db1be6
Jac0 <leey153@naver.com>
2026-02-15 21:53:33 +0900

    ✏️ Feat : Global Layout

--- src/app/router/routes.tsx
+++ src/app/router/routes.tsx
@@ -1,11 +1,13 @@
 import type { RouteObject } from 'react-router-dom'
 
+import { GlobalLayout } from '@app/components'
 import { HomePage, LoginPage, NotFoundPage } from '@app/pages'
 
 export const routes: RouteObject[] = [
   {
     path: '/',
-    element: <HomePage />,
+    element: <GlobalLayout />,
+    children: [{ index: true, element: <HomePage /> }],
   },
   {
     path: '/login',
