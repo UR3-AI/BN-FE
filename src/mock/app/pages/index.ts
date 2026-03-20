fe238687a3cdfadd2213e0e3e7212e29c1742f88
최수빈 <gucci9107@gmail.com>
2026-03-20 15:52:43 +0900

    ✏️ MOCK : 라우터에 Dashboard, To-Dos 페이지 추가

--- src/mock/app/pages/index.ts
+++ src/mock/app/pages/index.ts
@@ -1,5 +1,7 @@
+export * from "./dashboard";
 export * from "./editor";
 export * from "./graph";
 export * from "./login";
 export * from "./search";
 export * from "./settings";
+export * from "./todos";
--- src/mock/app/router/routes.tsx
+++ src/mock/app/router/routes.tsx
@@ -2,19 +2,23 @@ import type { RouteObject } from "react-router-dom";
 
 import { ProtectedRoute, PublicRoute } from "@app/router/provider";
 
+import { DashboardPage } from "@/mock/app/pages/dashboard";
 import { EditorPage } from "@/mock/app/pages/editor";
 import { GraphPage } from "@/mock/app/pages/graph";
 import { LoginPage } from "@/mock/app/pages/login";
 import { SearchPage } from "@/mock/app/pages/search";
 import { SettingsPage } from "@/mock/app/pages/settings";
+import { TodosPage } from "@/mock/app/pages/todos";
 
 export const routes: RouteObject[] = [
   {
     element: <ProtectedRoute />,
     children: [
-      { path: "/", element: <EditorPage /> },
+      { path: "/", element: <DashboardPage /> },
+      { path: "/editor", element: <EditorPage /> },
       { path: "/search", element: <SearchPage /> },
       { path: "/graph", element: <GraphPage /> },
+      { path: "/todos", element: <TodosPage /> },
       { path: "/settings", element: <SettingsPage /> },
     ],
   },
